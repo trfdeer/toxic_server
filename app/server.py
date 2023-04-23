2051874c36603774544a30044aa5fe95245679e2
Tuhin Tarafder <ttarafder7d1@protonmail.com>
2023-04-23 11:48:01 +0530

    updated predictions

--- app/server.py
+++ app/server.py
@@ -1,12 +1,16 @@
+from deep_translator import LibreTranslator
 from detoxify import Detoxify
+from dotenv import load_dotenv
 from flask import Flask, abort, jsonify, request
 from flask_cors import CORS
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-
-model = Detoxify("multilingual")
+translator = LibreTranslator(source="auto", target="en")
+model = Detoxify()
 
 
 @app.get("/text")
@@ -14,7 +18,11 @@ def get_prediction():
     comment_text = request.args.get("q")
     if comment_text is None:
         return abort(400)
-    return jsonify({"toxic": 1 if model.predict(comment_text)["toxicity"] >= 0.5 else 0})
+
+    translated_text = translator.translate(comment_text)
+    return jsonify(
+        {"toxic": 1 if model.predict(translated_text)["toxicity"] >= 0.5 else 0}
+    )
 
 
 app.run("127.0.0.1", 8888)
