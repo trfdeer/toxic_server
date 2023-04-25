55ffbfb2c83d302fccae80bd157786dbb90bd51d
Tuhin Tarafder <ttarafder7d1@protonmail.com>
2023-04-25 16:37:35 +0530

    remove translator

--- app/server.py
+++ app/server.py
@@ -1,4 +1,3 @@
-from deep_translator import LibreTranslator
 from detoxify import Detoxify
 from dotenv import load_dotenv
 from flask import Flask, abort, jsonify, request
@@ -9,7 +8,6 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-translator = LibreTranslator(source="auto", target="en")
 model = Detoxify()
 
 
@@ -19,9 +17,8 @@ def get_prediction():
     if comment_text is None:
         return abort(400)
 
-    translated_text = translator.translate(comment_text)
     return jsonify(
-        {"toxic": 1 if model.predict(translated_text)["toxicity"] >= 0.5 else 0}
+        {"toxic": 1 if model.predict(comment_text)["toxicity"] >= 0.5 else 0}
     )
 
 
