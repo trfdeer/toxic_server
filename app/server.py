7b1908afb45f32ccc815b525e768a005f1f05b3a
Tuhin Tarafder <ttarafder7d1@protonmail.com>
2023-05-14 08:02:15 +0530

    replace `detoxify` library with own models
    remove unneeded packages

--- app/server.py
+++ app/server.py
@@ -1,14 +1,36 @@
-from detoxify import Detoxify
+import joblib
 from dotenv import load_dotenv
 from flask import Flask, abort, jsonify, request
 from flask_cors import CORS
+from keras.models import load_model
+from keras.utils import pad_sequences
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-model = Detoxify()
+
+model_lstm = load_model("./saved/model")
+tokenizer = joblib.load("./saved/tokenizer")
+
+
+def predict(text):
+    data_tokenized = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=224)
+    pred = model_lstm.predict(data_tokenized)[0]
+    return dict(
+        zip(
+            [
+                "toxic",
+                "severe_toxic",
+                "obscene",
+                "threat",
+                "insult",
+                "identity_hate",
+            ],
+            pred,
+        )
+    )
 
 
 @app.get("/text")
@@ -17,9 +39,9 @@ def get_prediction():
     if comment_text is None:
         return abort(400)
 
-    return jsonify(
-        {"toxic": 1 if model.predict(comment_text)["toxicity"] >= 0.5 else 0}
-    )
+    preds = predict(comment_text)
+    print(preds)
+    return jsonify({"toxic": 1 if preds["toxic"] >= 0.5 else 0})
 
 
 app.run("127.0.0.1", 8888)
